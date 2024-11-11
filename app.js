2187f9902ac5988fcb75faa84b6dd8b17760b9d6
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2024-11-11 13:07:53 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,16 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
 
-app.use("/publisers", publisherRoute);
+app.use("/api/publisers", publisherRoute);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
